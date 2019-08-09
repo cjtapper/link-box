936ca37e1612a14c90e055d55a5bea83b76ec15b
Chris Tapper <chris@cjtapper.com>
2019-08-09 20:33:43 +0200

    Add method for extracting date from filename

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 
 from lxml import etree as ET
@@ -13,9 +14,10 @@ class Article:
     def title(self):
         return self._title
 
-    def __init__(self, path, title):
+    def __init__(self, path, title, saved_at):
         self._path = path
         self._title = title
+        self._saved_at = saved_at
 
 
 class ArticleFactory:
@@ -24,7 +26,8 @@ class ArticleFactory:
         with open(path) as file:
             html = ET.HTML(file.read())
         title = cls._get_title_from_html(html)
-        return Article(path, title)
+        saved_at = cls._get_timestamp_from_filename(path)
+        return Article(path, title, saved_at)
 
     @staticmethod
     def _get_title_from_html(html):
@@ -34,6 +37,10 @@ class ArticleFactory:
         else:
             return NO_TITLE
 
+    @staticmethod
+    def _get_timestamp_from_filename(path):
+        return datetime.strptime(path.stem.partition("_")[0], settings.TIMESTAMP_FORMAT)
+
 
 def main():
     p = Path(settings.ROOT_DIRECTORY) / "unread"
@@ -41,7 +48,7 @@ def main():
     files = p.glob("*.html")
     articles = [ArticleFactory.from_html_file(f) for f in files]
     for article in articles:
-        print(article.title)
+        print(article.title, article._saved_at, article._path)
 
 
 if __name__ == "__main__":
