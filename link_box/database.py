15cd841969e2b84f0b4a74b64a2a93a0069e8f3f
Chris Tapper <chris@cjtapper.com>
2019-09-15 20:52:29 +0200

    Change direction of development.
    I no longer want to use the filesystem and static HTML files. I will
    instead develop a small Flask application.

---
+++ link_box/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, DateTime, Integer, String, create_engine, func
+from sqlalchemy.ext.declarative import declarative_base, declared_attr
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine("sqlite:///:memory:", echo=True)
+Session = scoped_session(sessionmaker(bind=engine))
+
+
+class Base:
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    id = Column(Integer, primary_key=True)
+    created = Column(DateTime, default=func.now())
+    updated = Column(DateTime, onupdate=func.now())
+
+    query = Session.query_property()
+
+
+Base = declarative_base(cls=Base)
+
+
+class Article(Base):
+    title = Column(String, nullable=False)
+    source_url = Column(String, nullable=False)
+    html = Column(String, nullable=False)
